f7e0be577d0e431229dd4d2782b7cb0215433778
Ito <xiushu@ip-10-79-13-210.ap-northeast-1.compute.internal>
2025-11-21 15:30:35 +0900

    投稿ページ（セルフ）一段落

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,30 +1,5 @@
-import Image from "next/image";
-import MockData from "@/data/mock-data.json";
-import { SpotCard } from "@/components/features/spot/";
-import { Spot } from "@/types/spot/types";
-
-export default function Top() {
-  const spots = MockData as Spot[];
-
+export default function Page() {
   return (
-    <main>
-      <div>
-        {/* <Image 
-          src="/assets/logo/logo.png"
-          alt="Snap Spot"
-          width={88}
-          height={44}
-        /> */}
-        ここにロゴが入ります
-      </div>
-      <ul>
-        {spots.map((spot) => (
-          <li>
-            <SpotCard {...spot}/>
-          </li>
-        ))}
-      </ul>
-      <button></button>
-    </main>
-  );
+    <div>こちらは、sampleディレクトリーに配置したpage.tsxです。</div>
+  )
 }
