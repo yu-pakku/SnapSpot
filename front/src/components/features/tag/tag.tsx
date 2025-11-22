be0ec6ff407bbfd5deeab1597266beb272407852
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-22 23:43:21 +0900

    [add] Create Tag Component

---
+++ front/src/components/features/tag/tag.tsx
@@ -0,0 +1,10 @@
+import { FiTag } from "react-icons/fi"
+
+export function Tag({ name }: { name: string}) {
+  return (
+    <div className="flex gap-2 py-1 px-2 bg-castle-green500 text-white Body12Medium rounded-sm">
+      <FiTag size={16} color="white" />
+      <span>{name}</span>
+    </div>
+  )
+}
