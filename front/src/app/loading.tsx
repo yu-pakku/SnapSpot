6590041629d4ff9d5b0444b7ea6b9cfbd1ad9556
junpei-chan <jumpei24s1102270@gmail.com>
2025-11-23 12:04:37 +0900

    [add] Create Loading Page

--- front/src/app/loading.tsx
+++ front/src/app/loading.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image"
+
 export default function Loading() {
   return (
-    <main></main>
+    <main className="flex flex-col items-center justify-center gap-8">
+      <Image 
+        src="/loading.svg"
+        alt="loading"
+        width={196}
+        height={196}
+        className="animate-spin"
+      />
+      <h1 className="Heading24 text-castle-green200">
+        Generating the route...
+      </h1>
+    </main>
   )
 }
--- front/src/app/map/page.tsx
+++ front/src/app/map/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRef, useEffect, useState } from "react";
+import Loading from "../loading";
 import { IoIosArrowBack } from "react-icons/io";
 import { LangageMenu } from "@/components/shared";
 import mapboxgl from "mapbox-gl";
@@ -9,6 +10,7 @@ export default function Map() {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
   const [langage, setLangage] = useState("English");
   const [isActiveLang, setIsActiveLang] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
   const mapRef = useRef<mapboxgl.Map | null>(null);
   const mapContainerRef = useRef(null);
 
@@ -18,21 +20,28 @@ export default function Map() {
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current as HTMLElement,
       });
+      mapRef.current.on("load", () => {
+        setIsLoading(false);
+      });
     }
-
     return () => {
-      mapRef.current?.remove()
-    }
-  }, [])
+      mapRef.current?.remove();
+    };
+  }, []);
 
   return (
     <main>
+      {isLoading && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-70">
+          <Loading />
+        </div>
+      )}
       <div className="fixed flex top-0 left-0 bg-white w-screen h-14 items-center justify-between px-5 border-b-[0.5px] border-gray400 shadow-[0_2px_5px_-2px_rgba(0,0,0,0.25)] z-10">
         <IoIosArrowBack
           size={32}
           color="black"
         />
-        <LangageMenu 
+        <LangageMenu
           onSwitch={setIsDropdownVisible}
           onLangChange={setLangage}
           onActive={setIsActiveLang}
@@ -40,10 +49,10 @@ export default function Map() {
           isActive={isActiveLang}
         />
       </div>
-      <div 
+      <div
         ref={mapContainerRef}
         className="w-screen h-screen"
       />
     </main>
-  )
+  );
 }
